4dc618d9d95530196d03060a9c86051e44c30786
StandOut <StandOut.first@gmail.com>
2023-04-19 09:55:21 +0900

    jsp화면 띄우기 - build.gradle파일에 jsp사용을 위한 jstl라이브러리 추가, application.properties에 jsp파일 경로를 지정한다.

---
+++ src/main/java/com/myboot02/DemoController.java
@@ -0,0 +1,24 @@
+package com.myboot02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public  class DemoController {
+@ResponseBody
+  @RequestMapping("/") 
+  public String home(){
+    System.out.println("Hello Boot!!");
+    return "Hello Boot!!"; 
+  }
+
+  @RequestMapping("/hello.do")
+  public String hello(Model model){
+	System.out.println("Hi");
+	model.addAttribute("message","hello.jsp¿‘¥œ¥Ÿ.");
+	return "test/hello";
+  }
+}
+
