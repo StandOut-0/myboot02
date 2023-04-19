f7bca362a9f1941076e6598294b936a786303238
StandOut <StandOut.first@gmail.com>
2023-04-19 11:29:17 +0900

    타일즈 사용하기 - tilesConfig.java파일에 @Configuration설정파일로 지정 후  @Bean을 생성해 타일즈설정을 읽어들인다.

--- src/main/java/com/myboot02/member/vo/MemberVO.java
+++ src/main/java/com/myboot02/member/vo/MemberVO.java
@@ -5,6 +5,11 @@ import java.sql.Date;
 import org.apache.ibatis.type.Alias;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Component("memberVO")
 public class MemberVO {
 	private String id;
@@ -12,53 +17,4 @@ public class MemberVO {
 	private String name;
 	private String email;
 	private Date joinDate;
-
-	public MemberVO() {
-		System.out.println("MemberVO");
-	}
-	
-	
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getJoinDate() {
-		return joinDate;
-	}
-
-	public void setJoinDate(Date joinDate) {
-		this.joinDate = joinDate;
-	}
-
-	
-	
 }
